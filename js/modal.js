6e25bc01a95aa0a899c69d22ff54931c6ef24b9c
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2022-05-17 13:37:24 +0300

    Закрытие модального окна + стили

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,13 @@
 (() => {
     const refs = {
         openModalBtn: document.querySelector("[data-modal-open]"),
+        openModalBtnEducation: document.querySelector("[data-modal-open-education]"),
         closeModalBtn: document.querySelector("[data-modal-close]"),
         backdrop: document.querySelector("[data-backdrop]"),
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtnEducation.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
 
     function toggleModal() {
