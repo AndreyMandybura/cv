5e924267c7520360088bab5119e83249f7d456eb
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2022-05-19 12:41:09 +0300

    Функционал модалки

--- js/modal.js
+++ js/modal.js
@@ -6,11 +6,30 @@
         backdrop: document.querySelector("[data-backdrop]"),
     };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.openModalBtnEducation.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn.addEventListener("click", onOpenModal);
+    refs.openModalBtnEducation.addEventListener("click", onOpenModal);
+    refs.closeModalBtn.addEventListener("click", onCloseModal);
+    refs.backdrop.addEventListener('click', onBackdropClick);
 
-    function toggleModal() {
-        refs.backdrop.classList.toggle("is-hidden");
+    function onOpenModal() {
+        window.addEventListener('keydown', onEskKeyPress);
+        refs.backdrop.classList.remove("is-hidden");
+    }
+
+    function onCloseModal() {
+        window.removeEventListener('keydown', onEskKeyPress);
+        refs.backdrop.classList.add("is-hidden");
+    }
+
+    function onBackdropClick(event) {
+        if (event.currentTarget === event.target) {
+            refs.backdrop.classList.add("is-hidden");
+        }
+    }
+
+    function onEskKeyPress(event) {
+        if (event.code === 'Escape') {
+            refs.backdrop.classList.add("is-hidden");
+        }
     }
 })();
